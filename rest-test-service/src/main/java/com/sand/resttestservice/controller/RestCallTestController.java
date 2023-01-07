25cbb10aeeb1c81ddfe878446f8d92a60551dacf
SFernando <sandeepa@grubtech.com>
2023-01-07 13:57:15 +0530

    fixed compilation error

--- rest-test-service/src/main/java/com/sand/resttestservice/controller/RestCallTestController.java
+++ rest-test-service/src/main/java/com/sand/resttestservice/controller/RestCallTestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.sand.resttestservice.model.Test;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;Ø
+import lombok.extern.log4j.Log4j2;
 
 
 @Log4j2
